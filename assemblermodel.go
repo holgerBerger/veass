a40ac6a5e51ad3bfeeeceeee8b6358d82c09ef13
Holger Berger <holger.berger@googlemail.com>
2018-03-20 21:29:28 +0100

    first version of view and assembler model

---
+++ assemblermodel.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gdamore/tcell"
+)
+
+type assemblerModelT struct {
+	file       *FileBuffer
+	lastline   string // buffer last line
+	lastlinenr int    // buffer numbe rof last lien
+}
+
+func (a *assemblerModelT) GetCell(x, y int) (rune, tcell.Style) {
+	if y != a.lastlinenr || a.lastlinenr == 0 {
+		a.lastline = a.file.GetLine(y)
+		a.lastlinenr = y
+	}
+	return rune(a.lastline[x]), tcell.StyleDefault
+}
+
+func (a *assemblerModelT) GetNrLines() int {
+	block := a.file.appendin
+	return a.file.lineblocks[block].lastline
+}
+
+func (a *assemblerModelT) GetLineLen(line int) int {
+	return len(a.file.GetLine(line))
+}
--- main.go
+++ main.go
@@ -7,13 +7,13 @@ import (
 
 func main() {
 	var (
-		//assemblerfile *AssemblerFile
-		err error
+		assemblerfile *AssemblerFile
+		err           error
 	)
 
 	filename := os.Args[1]
 	if filename[len(filename)-2:] == ".s" {
-		_, err = NewAssemblerFile(filename)
+		assemblerfile, err = NewAssemblerFile(filename)
 		if err != nil {
 			panic(err)
 		}
@@ -22,5 +22,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	RunGUI()
+	assemblermodel := &assemblerModelT{file: assemblerfile.filebuffer}
+
+	RunGUI(assemblermodel)
 }
