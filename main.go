b1a0d800ed96aa85b719a2cd5cc02e93c8b40459
Holger Berger <holger.berger@googlemail.com>
2018-03-29 22:06:30 +0200

    profiling switch

--- main.go
+++ main.go
@@ -10,7 +10,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 
 	flags "github.com/jessevdk/go-flags"
 )
@@ -18,6 +21,7 @@ import (
 var version string
 
 var opts struct {
+	Profile    bool   `long:"profile" short:"p" description:"profile the application"`
 	Sourcedirs string `long:"sourcedirs" short:"s" description:"comma seperated list of directories to search for source files"`
 }
 
@@ -27,18 +31,20 @@ func main() {
 		err           error
 	)
 
-	/*
-		f1, err := os.Create("cpuprofile")
+	args, err := flags.Parse(&opts)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if opts.Profile {
+		f1, err := os.Create("veass.cpuprofile")
 		if err != nil {
 			log.Fatal("could not create CPU profile: ", err)
 		}
 		if err := pprof.StartCPUProfile(f1); err != nil {
 			log.Fatal("could not start CPU profile: ", err)
 		}
-		defer pprof.StopCPUProfile()
-	*/
-
-	args, err := flags.Parse(&opts)
+	}
 
 	if len(args) < 1 {
 		fmt.Println("veass version", version)
@@ -65,8 +71,9 @@ func main() {
 
 	tui.Run()
 
-	/*
-		f2, err := os.Create("memprofile")
+	if opts.Profile {
+		pprof.StopCPUProfile()
+		f2, err := os.Create("veass.memprofile")
 		if err != nil {
 			log.Fatal("could not create memory profile: ", err)
 		}
@@ -75,5 +82,5 @@ func main() {
 			log.Fatal("could not write memory profile: ", err)
 		}
 		f2.Close()
-	*/
+	}
 }
